b56920f0bb3761097f25d1c7bc38e2a4c89987b3
Saim Khan <sayemkhan2345@gmail.com>
2025-02-27 23:24:32 +0500

    Workin test

---
+++ WebWithEmptyProject/WebWithEmptyProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebWithEmptyProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
