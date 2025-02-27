25fc6807bfe50d296691afa2fa34ee94d2c5ca74
Saim Khan <sayemkhan2345@gmail.com>
2025-02-28 00:24:55 +0500

    Start CodeFirstApproch work firstday

---
+++ CodeFirstApproch/CodeFirstApproch/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstApproch.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Column("StudenName",TypeName ="varchar(100)")]
+        public string Name { get; set; }
+
+        [Column("FatherName", TypeName = "varchar(100)")]
+        public string FatherName { get; set; }
+
+        [Column("StudenGender", TypeName = "varchar(10)")]
+        public string Gender { get; set; }
+
+        [Column("GurdianContactNo")]
+        public int ContactNo { get; set; }
+
+        [Column("StudenClass", TypeName = "varchar(50)")]
+        public string Grade { get; set; }
+
+        [Column("StudenAge")]
+        public int Age { get; set; }
+    }
+}
---
+++ CodeFirstApproch/CodeFirstApproch/Models/StudentDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApproch.Models
+{
+    public class StudentDBContext : DbContext
+    {
+        //ctor
+
+        public StudentDBContext(DbContextOptions options) : base(options) 
+        {
+                
+        }
+
+        public DbSet<Student> Students   { get; set; }
+    }
+}
