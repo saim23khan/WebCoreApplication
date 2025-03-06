2aab29c2c1164668c94dec71090eb6a74f51a5b4
Saim Khan <sayemkhan2345@gmail.com>
2025-03-06 23:01:20 +0500

    Crud workin for efdit or delete

--- CodeFirstApproch/CodeFirstApproch/Controllers/HomeController.cs
+++ CodeFirstApproch/CodeFirstApproch/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using CodeFirstApproch.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
@@ -39,6 +40,7 @@ namespace CodeFirstApproch.Controllers
             {
               await  studentDB.Students.AddAsync(std);
               await  studentDB.SaveChangesAsync();
+                TempData["Create_Record"] = "Create Success..";
                 return RedirectToAction("Index","Home");
             }
 
@@ -59,6 +61,63 @@ namespace CodeFirstApproch.Controllers
             return View(stdData);
         }
 
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || studentDB.Students == null)
+            {
+                return NotFound();
+            }
+            var stdData = await studentDB.Students.FindAsync(id);
+            if (stdData == null)
+            {
+                return NotFound();
+            }
+            return View(stdData);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id , Student std)
+        {
+            if(id != std.ID)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                 studentDB.Update(std);
+                await studentDB.SaveChangesAsync();
+                TempData["Update_Record"] = "Update Success..";
+                return RedirectToAction("Index","Home");
+            }
+            return View(std);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null || studentDB.Students == null)
+            {
+                return NotFound();
+            }                
+            var std = await studentDB.Students.FirstOrDefaultAsync(x => x.ID == id);
+            if(std == null)
+            {
+                return NotFound();
+            }
+            return View(std);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var std =await studentDB.Students.FindAsync(id);
+            if (std != null)
+            {
+                studentDB.Students.Remove(std);
+            }
+            await studentDB.SaveChangesAsync();
+            TempData["Delete_Record"] = "Deleted Success..";
+            return RedirectToAction("Index", "Home");           
+        }
         public IActionResult Privacy()
         {
             return View();
