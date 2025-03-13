ded31242799a1769d87e4beedff0397afd0d082d
Saim Khan <sayemkhan2345@gmail.com>
2025-03-13 23:40:24 +0500

    Create a web App with the help of Razor controller

---
+++ WebCrudApplicationScaff/WebCrudApplicationScaff/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebCrudApplicationScaff.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string StudenName { get; set; } = null!;
+
+    public string FatherName { get; set; } = null!;
+
+    public string StudenGender { get; set; } = null!;
+
+    public long GurdianContactNo { get; set; }
+
+    public string StudenClass { get; set; } = null!;
+
+    public int StudenAge { get; set; }
+}
