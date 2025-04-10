e167edb23212381c22275d69b4be7530a5594102
Saim Khan <sayemkhan2345@gmail.com>
2025-04-11 00:42:16 +0500

    working for ddl with the help of mvc tag

--- LoginWorkWithTheHelpOFDBFirst/LoginWorkWithTheHelpOFDBFirst/Controllers/HomeController.cs
+++ LoginWorkWithTheHelpOFDBFirst/LoginWorkWithTheHelpOFDBFirst/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
 using LoginWorkWithTheHelpOFDBFirst.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace LoginWorkWithTheHelpOFDBFirst.Controllers
 {
+    public enum EnumGender
+    {
+        male,
+        Female
+    }
+
     public class HomeController : Controller
     {
         private readonly CreateDbforCodeContext context;
@@ -52,6 +59,44 @@ namespace LoginWorkWithTheHelpOFDBFirst.Controllers
             return View();
         }
 
+        private List<SelectListItem> GetGenderList()
+        {
+            return new List<SelectListItem>
+    {
+        new SelectListItem { Value = "0", Text = "Male" },
+        new SelectListItem { Value = "1", Text = "Female" }
+    };
+        }
+
+        public IActionResult CreateStdForm()
+        {
+            if (HttpContext.Session.GetString("AddSessionForLogin") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("AddSessionForLogin").ToString();
+                
+                ViewBag.Gender = GetGenderList();
+            }
+            else
+            {
+                return RedirectToAction("LoginPage", "Login");
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateStdForm(Student studencreate)
+        {
+            ViewBag.Gender = GetGenderList();
+            if (!ModelState.IsValid)
+            {
+                await context.Students.AddAsync(studencreate);
+                await context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "User created successfully!";
+                return RedirectToAction("CreateStdForm");
+            }
+
+            
+            return View(studencreate);
+        }
         public IActionResult Privacy()
         {
             if (HttpContext.Session.GetString("AddSessionForLogin") != null)
--- LoginWorkWithTheHelpOFDBFirst/LoginWorkWithTheHelpOFDBFirst/Models/User.cs
+++ LoginWorkWithTheHelpOFDBFirst/LoginWorkWithTheHelpOFDBFirst/Models/User.cs
@@ -7,12 +7,12 @@ namespace LoginWorkWithTheHelpOFDBFirst.Models;
 public partial class User
 {
     public int Id { get; set; }
-
+    [Required]
     public string UserName { get; set; } = null!;
-
+    [Required]
     [DataType(DataType.Password)]
     public string Password { get; set; } = null!;
-
+    [Required]
     public string Email { get; set; } = null!;
 
     public DateTime? CreatedAt { get; set; }
